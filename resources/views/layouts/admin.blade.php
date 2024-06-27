920dcc7d22d775eae06fac9fead2a6ee9a043606
Barrikin <54464128+Barrikin@users.noreply.github.com>
2024-06-27 17:24:03 -0400

    add date to dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -35,10 +35,23 @@
             <script type="text/javascript" charset="utf-8">
                 let a;
                 let time;
+                function getWeekNumber(d) {
+                    // Copy date so don't modify original
+                    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+                    // Set to nearest Thursday: current date + 4 - current day number
+                    // Make Sunday's day number 7
+                    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+                    // Get first day of year
+                    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+                    // Calculate full weeks to nearest Thursday
+                    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+                    // Return array of year and week number
+                    return [d.getUTCFullYear(), weekNo];
+                }
                 setInterval(() => {
                     a = new Date();
                     a.toLocaleString('en-US', { timeZone: 'America/New_York' })
-                    time = 'EST: ' + a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds() + ' Week: ' + a.getWeekNumber();
+                    time = 'EST: ' + a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds() + ' Week: ' + getWeekNumber(a);
                     document.getElementById('time').innerHTML = time;
                 }, 1000);
             </script>
